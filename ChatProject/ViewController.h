38d52518809866fe970dbe7bd3ff67bb8e4a56dd
xiaoguang <1259123804@qq.com>
2018-07-20 16:09:27 +0800

    Initial Commit

---
+++ ChatProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatProject
+//
+//  Created by Duke Li on 2018/7/20.
+//  Copyright © 2018年 Duke Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChatProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChatProject
+//
+//  Created by Duke Li on 2018/7/20.
+//  Copyright © 2018年 Duke Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
